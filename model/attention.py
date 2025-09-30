1277f3820ecef3734c48e1db0353027bb38e1e5f
Nico Dreessen <nico.dreessen93@gmail.com>
2025-09-30 21:45:46 +0200

    Implement Multi-head attention.

--- model/attention.py
+++ model/attention.py
@@ -25,4 +25,55 @@ def attention(query, key, value, mask=None, dropout=None):
     return torch.matmul(attn_logits, value), attn_logits
 
 class MultiHeadAttention(nn.Module):
-    pass
+    def __init__(self, h, d_model, dropout=0.1):
+        """Construct a multi-head attention module.
+
+        Args:
+            h: The number of attention heads to use in parallel.
+            d_model: The model input dimension.
+            dropout: The dropout rate to use.
+        """
+        super(MultiHeadAttention, self).__init__()
+        # Ensure that d_model is a multiple of h, such that we can
+        # evenly distribute the input among h heads.
+        assert d_model % h == 0
+        self.d_model = d_model
+        self.h = h
+        self.d_k = d_model // h
+        self.linears = clones(nn.Linear(d_model, d_model), 4)
+        self.attn = None
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, query, key, value, mask=None):
+        """
+        Push query, key and value sequences through the multi-head attention block.
+        Note that it is assume that d_k = d_v in the paper.
+
+        Args:
+            query: The query sequence tensor of shape (batch_size, seq_length, d_model).
+            key: The key sequence tensor of shape (batch_size, seq_length, d_model).
+            value: The value sequence tensor of shape (batch_size, seq_length, d_model).
+            mask: The mask to apply (if any) to prevent future positions to attend to earlier positions.
+
+        Returns:
+            The output tensor that results after applying multi-head attetion to query, key and value.
+        """
+        if mask is not None:
+            # Insert extra dimension to account for batch_size dimension
+            mask = torch.unsqueeze(mask, 1)
+        num_batches = query.size(0)
+      
+        # Step 1: Do batched linear projections, d_model => h x d_k
+        query, key, value = \
+            [linear(x).view(num_batches, -1, self.h, self.d_k).transpose(1, 2)
+             for linear, x in zip(self.linears, (query, key, value))]
+        
+        # Step 2: Apply attention to the batches
+        x, self.attn = attention(query, key, value, mask=mask, dropout=self.dropout)
+
+        # Step 3: Concat the tensors using a view and apply the final linear layer.
+        # h x d_k => d_model
+        x = x.transpose(1, 2).contiguous() \
+             .view(num_batches, -1, self.h * self.d_model)
+        
+        return self.linears[-1](x)
