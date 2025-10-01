fc3021a7bae41457b5857b4762882bfc5df92246
Nico Dreessen <nico.dreessen93@gmail.com>
2025-10-01 18:40:56 +0200

    Move position-wise feed-forward class to its own file.

---
+++ model/feed_forward.py
@@ -0,0 +1,27 @@
+#!/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+feed_forward.py: Implementation of the position-wise feed-forward neural network used in the encoder/decoder architecture, 
+as described in `The annotated transformer.`
+"""
+
+### Imports ###
+
+import torch.nn as nn
+
+class PositionWiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        """Construct a PositionWise feed-forward module which implements the FF equation.
+
+        Args:
+            d_model: The model input dimension size.
+            d_ff: The inner-layer dimensionality.
+        """
+        super(PositionWiseFeedForward, self).__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(p=dropout) 
+
+    def forward(self, x):
+        return self.linear_2(self.linear1(x).relu())
