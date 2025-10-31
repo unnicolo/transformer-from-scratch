cde5660a053caa728580ddbbf92d6a7cc558d4b2
Nico Dreessen <nico.dreessen93@gmail.com>
2025-10-31 12:45:45 +0100

    Add method for synthetic training data generation.

---
+++ data/synthetic_data.py
@@ -0,0 +1,35 @@
+#!/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+batch.py: Defines the Batch class for storing source and target sentences
+and constructing the corresponding attention masks.
+"""
+
+### IMPORTS ###
+import torch
+
+from data.batch import Batch
+
+### CONSTANTS ###
+PAD = 0
+SEQ_LEN = 10
+
+def generate_data(vocabulary_size, batch_size, num_batches):
+    """Generate synthetic data for a src-tgt copy task.
+
+    Args:
+        vocabulary_size: The size of the vocabulary to draw from.
+        batch_size: The size of a single batch.
+        num_batches: The number of batches to generate.
+
+    Returns:
+        A generator, yielding :class:`Batch` objects that hold on to a single batch each.
+    """
+    for _ in range(num_batches):
+        data = torch.randint(1, vocabulary_size, size=(batch_size, SEQ_LEN))
+        # Each data sequence starts with the same symbol - much like a <start-of-sequence>-token.
+        data[:, 0] = 1
+        src = data.clone().detach()
+        tgt = data.clone().detach()
+        yield Batch(src, tgt, PAD)
