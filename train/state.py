bd0a01fca7033c780097375504403022b33c9da0
Nico Dreessen <nico.dreessen93@gmail.com>
2025-11-03 19:08:24 +0100

    Improve name of variable.

--- train/state.py
+++ train/state.py
@@ -8,7 +8,7 @@ state.py: Keep track of different training metrics.
 class TrainState:
     """Track number of steps, examples and tokens processed."""
 
-    steps: int = 0          # Steps in the current epoch.
+    step: int = 0          # Steps in the current epoch.
     accum_step: int = 0     # Number of gradient calculation steps.
     tokens: int = 0         # Total number of tokens processed.
     samples: int = 0        # Total number of examples used.
