763e7f33b686e9e22f4dc8f6a944d8912a33373f
Nico Dreessen <nico.dreessen93@gmail.com>
2025-11-06 09:43:55 +0100

    Implement run_epoch method.

--- train/train.py
+++ train/train.py
@@ -15,15 +15,21 @@ from train.state import TrainState
 from data.batch import Batch
 from model.transformer import Transformer
 
+### CONSTANTS ###
+STEPS_UNTIL_LOG_WITHIN_EPOCH = 40 # number of steps within an epoch until we log metrics
+MODE_TRAIN = 'train'
+MODE_TRAIN_AND_LOG = 'train+log'
+LEARNING_RATE_PARAM_GROUP_SPECIFIER = 'lr'
+
 def run_epoch(
     data_iter: Generator[Batch, None, None],
     model: Transformer,
     loss_compute,
     optimizer: Optimizer,
     scheduler: Optimizer,
-    mode='train',
-    accum_iter = 1,
-    train_state=TrainState()
+    mode: str = MODE_TRAIN,
+    accum_iter: int = 1,
+    train_state: TrainState =TrainState()
 ):
     """Train the model for a single epoch.
     
@@ -36,7 +42,11 @@ def run_epoch(
         mode: Current training mode; either `train` or `train+log`
         accum_iter: The number of passes to perform after which we update the parameters
         train_state: The current state of the training process
+    
+    Returns:
+        (Tuple): The overall loss and the final train state.
     """
+
     start_time = time.time()
     total_tokens = 0
     total_loss = 0
@@ -54,7 +64,7 @@ def run_epoch(
         # Step 3: Compute the loss
         loss, loss_node = loss_compute(out, batch.tgt_y, batch.num_tokens)
 
-        if mode == 'train' or mode == 'train+log':
+        if mode == MODE_TRAIN or mode == MODE_TRAIN_AND_LOG:
             # Step 4: Backpropagate the gradients
             loss_node.backward()
             train_state.step += 1
@@ -67,3 +77,22 @@ def run_epoch(
                 num_accumulated += 1
                 train_state.accum_step += 1
             scheduler.step()
+
+        total_loss += loss
+        total_tokens += batch.ntokens
+        tokens += batch.ntokens
+
+        if i % STEPS_UNTIL_LOG_WITHIN_EPOCH == 1 and (mode == MODE_TRAIN or mode == MODE_TRAIN_AND_LOG):
+            lr = optimizer.param_groups[0][LEARNING_RATE_PARAM_GROUP_SPECIFIER]
+            elapsed_time = time.time() - start_time
+            print(
+                (
+                    "Epoch Step: %6d | Accumulation Step: %3d | Loss: %6.2f "
+                    + "| Tokens / Sec: %7.1f | Learning Rate: %6.1e"
+                )
+                % (i, num_accumulated, (loss / batch.ntokens), (tokens / elapsed_time), lr)
+            )
+            start_time = time.time()
+            del loss
+            del loss_node
+        return (total_loss / total_tokens), train_state
