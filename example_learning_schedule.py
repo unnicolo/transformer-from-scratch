5d0f77352d3468ad40acd9270c61c83793e236c8
Nico Dreessen <nico.dreessen93@gmail.com>
2025-11-17 18:28:27 +0100

    Add learning rate scheduler test script and update requirements file.

--- example_learning_schedule.py
+++ example_learning_schedule.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!./env/bin/python
 # -*- coding: UTF-8 -*-
 
 """
@@ -15,7 +15,10 @@ from torch.optim.lr_scheduler import LambdaLR
 
 from train.rate import rate
 
+import sys
+
 ### CONSTANTS ###
+FIGURE_WIDTH = 600
 STEPS_PER_EPOCH = 20000
 BETAS = (0.9, 0.98)
 EPSILON = 1e-9
@@ -39,11 +42,11 @@ def example_learning_schedule():
         ) 
         lr_scheduler = LambdaLR(optimizer, lr_lambda=lambda step: rate(step, *example))
 
-    history = []
-    for step in range(STEPS_PER_EPOCH):
-        history.append(optimizer.param_groups[0][LEARNING_RATE_KEY])
-        optimizer.step()
-        lr_scheduler.step()
+        history = []
+        for step in range(STEPS_PER_EPOCH):
+            history.append(optimizer.param_groups[0][LEARNING_RATE_KEY])
+            optimizer.step()
+            lr_scheduler.step()
         learning_rates.append(history)
     
     learning_rates = torch.tensor(learning_rates)
@@ -51,6 +54,27 @@ def example_learning_schedule():
     # Disable the row limit in Altair
     alt.data_transformers.disable_max_rows()
 
-    examples_data = pd.DataFrame(
-
+    examples_data = pd.concat(
+        [
+            pd.DataFrame(
+                {
+                    'step': range(STEPS_PER_EPOCH),
+                    'Learning Rate': learning_rates[warmup_index, :],
+                    'info': ["512:4000", "512:8000", "256:4000"][warmup_index],
+                }
+            )
+            for warmup_index in range(len(examples))
+        ]
     )
+
+    return (
+        alt.Chart(examples_data)
+        .mark_line()
+        .properties(width=FIGURE_WIDTH)
+        .encode(x='step', y='Learning Rate', color='info')
+        .interactive()
+    )
+
+if __name__ == '__main__':
+    chart = example_learning_schedule()
+    chart.save("Chart.html")
