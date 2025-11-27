09b0044abc6117c66c585dcbf13bea079c4c5153
Nico Dreessen <nico.dreessen93@gmail.com>
2025-11-27 20:31:16 +0100

    Implement loss module.

--- train/loss.py
+++ train/loss.py
@@ -5,7 +5,31 @@ loss.py: Loss computation module
 """
 
 ### IMPORTS ###
+import torch
 import torch.nn as nn
 
 class SimpleLossCompute(nn.Module):
-    pass
+    """A simple loss compute and train function."""
+    def __init__(self, generator, criterion):
+        """
+        Initialize the loss compute module.
+
+        Args:
+            generator: Probability output generator.
+            criterion: Loss criterion.
+        """
+        self.generator = generator
+        self.criterion = criterion
+
+    def __call__(self, x, y, norm):
+        """
+        
+        """
+        x = self.generator(x)
+        sloss = (
+            self.criterion(
+                x.contiguous().view(-1, x.size(-1)), y.contiguous().view(-1)
+            )
+            / norm
+        )
+        return sloss.data * norm, sloss
